9beaa12c3aeb8d67aa3c999cbc3d6a7b6ac830e3
allanlevsen <allanlevsen@gmail.com>
2026-06-01 02:06:01 -0600

    Convert ulong converter to a UInt128Converter

--- BitScheduleApi/Utility/ULongConverter.cs
+++ BitScheduleApi/Utility/ULongConverter.cs
@@ -4,18 +4,18 @@
     using System.Text.Json;
     using System.Text.Json.Serialization;
 
-    public class ULongConverter : JsonConverter<ulong>
+    public class UInt128Converter : JsonConverter<UInt128>
     {
-        public override ulong Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override UInt128 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             // Implement reading if necessary (here we assume numbers are provided as strings)
             // You could also call reader.GetUInt64() if your JSON contains numeric values.
             //
-            string s = reader.GetString();
-            return ulong.Parse(s);
+            string s = reader.GetString()!;
+            return UInt128.Parse(s);
         }
 
-        public override void Write(Utf8JsonWriter writer, ulong value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, UInt128 value, JsonSerializerOptions options)
         {
             // Write the ulong as a full decimal string.
             // This ensures that even very large ulong values are correctly represented in JSON without loss of precision.
